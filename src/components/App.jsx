474fb0b3a20c31730ffbee5711b588061f5ab428
devesh-bhushan <deepubhushan123@gmail.com>
2022-02-07 19:35:17 +0530

    made the structure of the project

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./Header"
+import Footer from "./Footer"
+
+function App (){
+    return<>
+        <Header/>
+        <Footer/>
+    </>
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+function Footer(){
+    return <>
+        <h1>Footer bar</h1> 
+    </>
+}
+export default Footer;
